930355e1121a0ffcbed2859389d5705a8bd15808
LavaCandy1 <aayush.garg.1793@gmail.com>
2024-09-16 16:18:26 +0530

    Updated

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://postgres:root@localhost:5432/QuizApp"
+URL_DATABASE = "postgresql://<username>:<password>@localhost:5432/QuizApp"
+# Replace <username>, <password>, and QuizApp with your PostgreSQL credentials.
 
 engine = create_engine(URL_DATABASE)
 
